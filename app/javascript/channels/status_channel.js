ef2f89c35c22ad7bd08eb38b70ecf5a7d69a0994
Alysson A Costa <alysson.avila.costa@gmail.com>
2019-12-22 23:17:20 -0300

    Status update feature (ActionCable)

---
+++ app/javascript/channels/status_channel.js
@@ -0,0 +1,18 @@
+import consumer from "./consumer"
+
+document.addEventListener("turbolinks:load", () => {
+  const contactHeader = document.querySelector("div.contact-header")
+  consumer.subscriptions.create({channel: "StatusChannel", user: contactHeader.dataset.user}, {
+    connected() {
+      console.log("Connected at StatusChannel")
+    },
+
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      document.querySelector("div.contact-header div.status").innerHTML = data["status"]
+    }
+  });
+})
